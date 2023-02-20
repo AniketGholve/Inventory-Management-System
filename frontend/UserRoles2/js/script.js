006d9ba2a6b901b0673d7bd634c8717416aa561b
Rishabh <t_varun@persistent.com>
2023-02-20 15:43:12 +0530

    Elp success and error orders

---
+++ frontend/UserRoles2/js/script.js
@@ -0,0 +1,57 @@
+
+
+app.controller("elp", ['$scope', '$http', function ($scope, $http) {
+   console.log("Hello")
+    $scope.tabs = [{
+        title: 'Success Orders',
+        url: 'successOrders.html'
+    }, {
+        title: 'Error Orders',
+        url: 'errorOrders.html'
+    }];
+    $scope.currentTab = 'elp_users.html'
+
+    $scope.onClickTab = function (tab) {
+        $scope.currentTab = tab.url;
+    }
+
+    $scope.isActiveTab = function (tabUrl) {
+        return tabUrl == $scope.currentTab;
+    }
+
+    $http({
+        method: 'GET',
+        url: 'http://localhost:7890/api/SuccessOrders/1',
+        headers: {
+           'Content-Type': 'application/json',
+           'Authorization': sessionStorage.getItem("token")
+        }
+    }).then((response) => {
+        console.log(response.data);
+
+        $scope.success_data = response.data;
+    },
+        (error) => {
+
+            console.log(error);
+
+        });
+
+        $http({
+            method: 'GET',
+            url: 'http://localhost:7890/api/ErrorOrders/2',
+            headers: {
+               'Content-Type': 'application/json',
+               'Authorization': sessionStorage.getItem("token")
+            }
+        }).then((response) => {
+        console.log(response.data);
+
+        $scope.error_data = response.data;
+    },
+        (error) => {
+
+            console.log(error);
+
+        });
+}]);
