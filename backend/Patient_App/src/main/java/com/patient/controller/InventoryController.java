0b13ee05ef07f5c5664547f78017b987447d1ded
Rishabh <rishabh@gmail.com>
2023-02-22 16:20:56 +0530

    username

--- backend/Patient_App/src/main/java/com/patient/controller/InventoryController.java
+++ backend/Patient_App/src/main/java/com/patient/controller/InventoryController.java
@@ -2,8 +2,6 @@ package com.patient.controller;
 
 
 import java.util.List;
-
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
