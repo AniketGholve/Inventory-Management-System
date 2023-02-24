709698692ad14e95a3193476758a030e224bda10
Rishabh <rishabh@gmail.com>
2023-02-24 22:37:22 +0530

    added enterprise api

--- backend/Patient_App/src/main/java/com/patient/ServiceImpl/EnterpriseServiceImpl.java
+++ backend/Patient_App/src/main/java/com/patient/ServiceImpl/EnterpriseServiceImpl.java
@@ -1,5 +1,36 @@
 package com.patient.ServiceImpl;
 
-public class EnterpriseServiceImpl {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+
+import com.patient.Repo.EnterpriseRepo;
+import com.patient.Service.Enterprise;
+import com.patient.Service.EnterpriseService;
+import com.patient.Service.List;
+
+public class EnterpriseServiceImpl implements EnterpriseService {
+	
+	
+	
+	@Autowired
+	private EnterpriseRepo enterpriseRepo;
+	
+private Enterprise createEnterprise(Enterprise enterprise){
+	Enterprise e=enterpriseRepo.save(enterprise)
+	return e;
+	
+}
+	
+	private ResponseEntity<List<Enterprise>> getAllEnterprise();
+	
+	private ResponseEntity<Enterprise> updateEnterprise(Enterprise enterprise);
+	
+	private ResponseEntity<String> deleteenterprise(Integer enterpriseId);
+	
+	
+	
+	
+	
+	
 
 }
