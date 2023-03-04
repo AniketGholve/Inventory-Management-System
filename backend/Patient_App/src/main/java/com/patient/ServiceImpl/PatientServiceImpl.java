c4cc3513f71b6e6cf1bac00255510c91ea4c2491
Swarnavo Roy <roybabai15999@gmail.com>
2023-03-04 23:10:37 +0530

    Added Clinic and inventory functionality

--- backend/Patient_App/src/main/java/com/patient/ServiceImpl/PatientServiceImpl.java
+++ backend/Patient_App/src/main/java/com/patient/ServiceImpl/PatientServiceImpl.java
@@ -98,34 +98,14 @@ public class PatientServiceImpl implements PatientService {
 	@Override
 	public List<Patient> getPatientByLocationId(Integer clinicLocationId) {
 		// TODO Auto-generated method stub
-		Query q=entityManager.createNativeQuery("select p from Patient p where p.location_id=:u" );
+		Query q=entityManager.createQuery("select p from Patient p where p.patientLocationId=:u" );
+
 		q.setParameter("u", clinicLocationId);
-		List<Object[]> l=q.getResultList();
-		List<Patient> resultList=new ArrayList<>();
-		for(Object [] o:l)
-		{
-			Patient p=new Patient();
-			p.setId((Integer)o[0]);
-			p.setIndependentInventory((Boolean) o[1]);
-			p.setPatientCreatedOn((Date) o[2]);
-			p.setPatientDob((Date) o[3]);
-			p.setPatientEmail((String) o[4]);
-			p.setPatientEnterpriseId((Integer) o[5]);
-			p.setPatientFirstName((String) o[6]);
-			p.setPatientId((String) o[7]);
-			p.setPatientLastDispenseId((Integer)o[8]);
-			p.setPatientLastName((String)o[9]);
-			p.setPatientLocationId((Integer)o[10]);
-			p.setPatientMiddleName((String)o[11]);
-			p.setPatientModifiedOn((Date)o[12]);
-			p.setPatientPaNeeded((Boolean)o[13]);
-			p.setPatientPayerType((String)o[14]);
-			p.setPatientSrcId((Integer)o[15]);
-			p.setPatientStatus((String)o[16]);
-			resultList.add(p);
-		}
-		return resultList;
-	}
+
+		List<Patient> l=q.getResultList();
+		 
+
+		return l;	}
 
 
 
