6f727636ca8477cb6a867f0f014097fef5ed8b41
Aniket Gholve <aniket_gholve@persistent.com>
2023-03-09 09:32:35 +0530

    added pop up in order alp

--- frontend/UserRoles2/js/app.js
+++ frontend/UserRoles2/js/app.js
@@ -481,18 +481,19 @@ app.controller("alp", ($scope, $http, $window) => {
         });
     }
     $scope.viewInventory=(productId,locationId)=>{
-        $http({
-            method: 'GET',
-            url: 'http://localhost:7890//getinventoryByProductId/'+productId+"/"+locationId,
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': sessionStorage.getItem("token")
-            }
-        }).then((response) => {
-            $scope.orderInventoryData=response.data;
-        }, (error) => {
-            console.log(error);
-        });
+        console.log(productId +" "+locationId)
+        // $http({
+        //     method: 'GET',
+        //     url: 'http://localhost:7890/getinventoryByProductId/'+productId+"/"+locationId,
+        //     headers: {
+        //         'Content-Type': 'application/json',
+        //         'Authorization': sessionStorage.getItem("token")
+        //     }
+        // }).then((response) => {
+        //     $scope.orderInventoryData=response.data;
+        // }, (error) => {
+        //     console.log(error);
+        // });
     }
 });
 
