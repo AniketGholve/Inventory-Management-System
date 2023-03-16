42ae70f3ad35ebf4d561e3594ba16f5d80cdf2c7
T Sai Varun <t_varun@persistent.com>
2023-03-16 16:41:49 +0530

    DispenceToPatient

--- backend/Patient_App/src/main/java/com/patient/controller/DispenceToPatientController.java
+++ backend/Patient_App/src/main/java/com/patient/controller/DispenceToPatientController.java
@@ -6,8 +6,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.patient.Entity.DispenceToPatient;
 import com.patient.Entity.Product;
 import com.patient.ServiceImpl.DispenceToPatientServiceImpl;
 @RestController
@@ -24,6 +27,13 @@ public class DispenceToPatientController {
 		
 		Product p=dispenceToPatientServiceImpl.getProductBySerialId(serialId);
 		return new ResponseEntity<Product>(p,HttpStatus.OK);
+		
+	}
+	
+	@PostMapping("/createDispense")
+	public ResponseEntity<DispenceToPatient> createDispense(@RequestBody DispenceToPatient dispenceToPatient){
+		DispenceToPatient dp = dispenceToPatientServiceImpl.createDispence(dispenceToPatient);
+		return new ResponseEntity<DispenceToPatient>(dp,HttpStatus.CREATED);
 		
 	}
 
