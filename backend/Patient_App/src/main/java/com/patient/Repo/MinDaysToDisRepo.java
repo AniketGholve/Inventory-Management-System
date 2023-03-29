cf25f095e773e5d15ef81a7ced0e42e3c3d2590f
T Sai Varun <t_varun@persistent.com>
2023-03-29 20:04:51 +0530

    Dropdown

---
+++ backend/Patient_App/src/main/java/com/patient/Repo/MinDaysToDisRepo.java
@@ -0,0 +1,11 @@
+package com.patient.Repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.patient.Entity.MinDaysToDis;
+
+@Repository
+public interface MinDaysToDisRepo extends JpaRepository<MinDaysToDis, Integer> {
+
+}
