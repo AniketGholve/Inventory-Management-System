454cc2a8928fb831333d1ddcd5cdeb3db120f0b2
Swarnavo Roy <roybabai15999@gmail.com>
2023-03-30 10:29:56 +0530

    Added Dispense Restrictions

--- backend/Patient_App/src/main/java/com/patient/controller/MinDaysToDisController.java
+++ backend/Patient_App/src/main/java/com/patient/controller/MinDaysToDisController.java
@@ -1,24 +1,24 @@
-package com.patient.controller;
-
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.patient.Entity.MinDaysToDis;
-import com.patient.ServiceImpl.MinDayToDisServiceImpl;
-
-@RestController
-public class MinDaysToDisController {
-	
-	@Autowired
-	private MinDayToDisServiceImpl minDayToDisServiceImpl;
-	
-	@GetMapping("/getAllMin")
-	public List<MinDaysToDis> getAllMin(){
-		List<MinDaysToDis> list = minDayToDisServiceImpl.getAll();
-		return list;
-	}
-	
-}
+//package com.patient.controller;
+//
+//import java.util.List;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//import com.patient.Entity.MinDaysToDis;
+//import com.patient.ServiceImpl.MinDayToDisServiceImpl;
+//
+//@RestController
+//public class MinDaysToDisController {
+//	
+//	@Autowired
+//	private MinDayToDisServiceImpl minDayToDisServiceImpl;
+//	
+//	@GetMapping("/getAllMin")
+//	public List<MinDaysToDis> getAllMin(){
+//		List<MinDaysToDis> list = minDayToDisServiceImpl.getAll();
+//		return list;
+//	}
+//	
+//}
