aeaae9514b41e0676f70e65bf49bfe9aca0b258d
T Sai Varun <t_varun@persistent.com>
2023-05-23 11:19:29 +0530

    Keycloak Configuartion

--- backend/Patient_App/src/main/java/com/patient/controller/AuthController.java
+++ backend/Patient_App/src/main/java/com/patient/controller/AuthController.java
@@ -1,64 +1,64 @@
-package com.patient.controller;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.patient.Entity.JwtRequest;
-import com.patient.Entity.JwtResponse;
-import com.patient.Security.CustomUserDetailService;
-import com.patient.Security.JwtUtilTokenHelper;
-
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-
-@RestController
-@CrossOrigin
-public class AuthController {
-
-	@Autowired
-	private CustomUserDetailService customUserDetailService;
-
-	@Autowired
-	private AuthenticationManager authenticationManager;
-
-	@Autowired
-	private JwtUtilTokenHelper jwtUtilTokenHelper;
-
-	@PostMapping("/login")
-	public ResponseEntity<JwtResponse> createToken(@RequestBody JwtRequest jwtRequest) throws Exception {
-		try {
-			this.authenticationManager.authenticate(
-					new UsernamePasswordAuthenticationToken(jwtRequest.getUsername(), jwtRequest.getPassword()));
-		} catch (UsernameNotFoundException e) {
-			e.printStackTrace();
-			throw new Exception("User Not Found Exception");
-		} catch (BadCredentialsException e) {
-			e.printStackTrace();
-			throw new Exception("Bad Credential");
-		}
-
-		UserDetails userDetails = this.customUserDetailService.loadUserByUsername(jwtRequest.getUsername());
-
-		String token = this.jwtUtilTokenHelper.generateToken(userDetails);
-		// {"token":"value"}
-		System.out.println(token);
-
-		return ResponseEntity.ok(new JwtResponse(token));
-
-	}
-	
-	@PostMapping("/logout")
-	public String logoutDo(HttpServletRequest request,HttpServletResponse response) {
-		return "redirect/login";
-	}
-
-}
+//package com.patient.controller;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.security.authentication.AuthenticationManager;
+//import org.springframework.security.authentication.BadCredentialsException;
+//import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+//import org.springframework.security.core.userdetails.UserDetails;
+//import org.springframework.security.core.userdetails.UsernameNotFoundException;
+//import org.springframework.web.bind.annotation.CrossOrigin;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestBody;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//import com.patient.Entity.JwtRequest;
+//import com.patient.Entity.JwtResponse;
+//import com.patient.Security.CustomUserDetailService;
+//import com.patient.Security.JwtUtilTokenHelper;
+//
+//import jakarta.servlet.http.HttpServletRequest;
+//import jakarta.servlet.http.HttpServletResponse;
+//
+//@RestController
+//@CrossOrigin
+//public class AuthController {
+//
+//	@Autowired
+//	private CustomUserDetailService customUserDetailService;
+//
+////	@Autowired
+////	private AuthenticationManager authenticationManager;
+//
+//	@Autowired
+//	private JwtUtilTokenHelper jwtUtilTokenHelper;
+//
+//	@PostMapping("/login")
+//	public ResponseEntity<JwtResponse> createToken(@RequestBody JwtRequest jwtRequest) throws Exception {
+//		try {
+////			this.authenticationManager.authenticate(
+////					new UsernamePasswordAuthenticationToken(jwtRequest.getUsername(), jwtRequest.getPassword()));
+//		} catch (UsernameNotFoundException e) {
+//			e.printStackTrace();
+//			throw new Exception("User Not Found Exception");
+//		} catch (BadCredentialsException e) {
+//			e.printStackTrace();
+//			throw new Exception("Bad Credential");
+//		}
+//
+//		UserDetails userDetails = this.customUserDetailService.loadUserByUsername(jwtRequest.getUsername());
+//
+//		String token = this.jwtUtilTokenHelper.generateToken(userDetails);
+//		// {"token":"value"}
+//		System.out.println(token);
+//
+//		return ResponseEntity.ok(new JwtResponse(token));
+//
+//	}
+//	
+//	@PostMapping("/logout")
+//	public String logoutDo(HttpServletRequest request,HttpServletResponse response) {
+//		return "redirect/login";
+//	}
+//
+//}
